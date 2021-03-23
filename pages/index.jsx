d025fe61f8a1d92510117b1d4089783d803bb651
Christian Raymond Wijaya <raymond_wijaya10@yahoo.co.id>
2021-03-23 11:12:41 +0700

    test2dari raymond

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,8 +5,8 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      
+    <div >
+      <p>asdas</p>
     </div>
   );
 }
