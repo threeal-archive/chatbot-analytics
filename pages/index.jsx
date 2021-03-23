762fdb518d87c37e7ca969d1e89a6153f821f4e9
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-03-23 12:08:31 +0700

    Fix lint

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,8 @@
-import Head from "next/head";
 import React from "react";
-
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <p>asdas</p>
     </div>
   );
